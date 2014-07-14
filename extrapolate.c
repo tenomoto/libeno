4166cc3baeb8669e6b7c43f404c402437e545d95
Takeshi Enomoto <takeshi.enomoto@gmail.com>
2014-07-14 16:59:33 +0900

    extrapolate.c: important bug fix. use Ts calculated at the lowest sigma level

--- extrapolate.c
+++ extrapolate.c
@@ -11,42 +11,41 @@ double calc_y
     double sig    /// < [in] full-level p/ps just above the surface
   )
 {
-  const double Rd = eno_air_Rd;
-  return gamma * Rd * log(sig);
+  const double rg = eno_air_Rd / eno_earth_gravity;
+  return gamma * rg * log(sig);
 }
 
-double calc_Ts /// calculate surface temperature
+double eno_extrapolate_Ts /// calculate surface temperature
   (
-    double T,  /// < [in] full-level temperature just above the surface
-    double sig /// < [in] full-level p/ps just above the surface
+    double Tl,  /// < [in] full-level temperature just above the surface
+    double sigl /// < [in] full-level p/ps just above the surface
   )
 {
-  const double rgg = eno_isa_dTdz(0)*eno_air_Rd/eno_earth_gravity;
-  return T * (1.0 + rgg*(sig - 1.0));
+  const double rgg = eno_isa_dTdz(0) * eno_air_Rd / eno_earth_gravity;
+  return Tl * (1.0 + rgg*(sigl - 1.0));
 }
 
 double eno_extrapolate_z /// extrapolate geopotential height
   (
-    double T,   /// < [in] full-level temperature just above the surface
-    double sig, /// < [in] full-level p/ps just above the surface
-    double zs   /// < [in] surface geopotential height, m
+    double zs, /// < [in] surface geopotential height, m
+    double Ts, /// < [in] surface temperature
+    double sig /// < [in] full-level p/ps
   )
 {
   const double rdg = eno_air_Rd / eno_earth_gravity;
   double gamma = -eno_isa_dTdz(0);
   double y = calc_y(gamma, sig);
-  return zs - rdg * calc_Ts(T, sig) * log(sig) * (1.0 + (0.5 + y / 0.6) * y);
+  return zs - rdg * Ts * log(sig) * (1.0 + (0.5 + y / 0.6) * y);
 }
 
 double eno_extrapolate_T /// extrapolate temperature
   (
-    double T,   /// < [in] full-level temperature just above the surface
-    double sig, /// < [in] full-level p/ps just above the surface
-    double zs   /// < [in] surface geopotential height, m
+    double zs, /// < [in] surface geopotential height, m
+    double Ts, /// < [in] surface temperature
+    double sig /// < [in] full-level p/ps
   )
 {
   double gamma = -eno_isa_dTdz(0);
-  double Ts = calc_Ts(T, sig);
   if (zs > 2000.0) {
     double T1 = Ts + gamma * zs;
     double T0 = MIN(T1, 298.0);
